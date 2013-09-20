2526572ae15dc81dd47a19e1c60025329e141a07
Michele Laramore <xandermansmom@fullsail.edu>
2013-09-19 21:39:22 -0500

    Week 3

--- android/assets/www/cordova_plugins.js
+++ android/assets/www/cordova_plugins.js
@@ -1,3 +1,102 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = []
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.core.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.core.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/Position.js",
+        "id": "org.apache.cordova.core.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.core.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.core.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/Camera.js",
+        "id": "org.apache.cordova.core.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.core.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.core.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.inappbrowser/www/InAppBrowser.js",
+        "id": "org.apache.cordova.core.inappbrowser.InAppBrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.core.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.globalization/www/GlobalizationError.js",
+        "id": "org.apache.cordova.core.globalization.GlobalizationError",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.globalization/www/globalization.js",
+        "id": "org.apache.cordova.core.globalization.globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    }
+]
 });
--- android/assets/www/js/main.js
+++ android/assets/www/js/main.js
@@ -1,49 +1,198 @@
 //Michele Laramore
 //AVF 1309
 
+
+
 //INSTAGRAM
- 
-  $('#instagram').on('pageinit', function() {
-
-    var tag = "dishes_pics",
-
-         url ="https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=dd55a33c284e4d99b2f703f3e2bdaf53",
-    
-
-        screenOutput = function(info){
 
 
-        console.log(info);
-
-
-    $("#data-msg").html("<h2>Instagram results:</h2>");
-
-
-    $.each(info.data, function(index, photo) {
-
-var pic = "<figure><img src='" + photo.images.standard_resolution.url + "'alt='" + photo.user.id + "' />" + "<h3>" + "<figcaption>" + photo.user.full_name  + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + photo.likes.count + "&nbsp;" + "&hearts;" + "'s" + "</figcaption>" + " </h3>" + "</figure>";        
-        $("#data-output").append(pic);
-    }); 
-};
-        $.getJSON(url, screenOutput); 
-
-    
-});
+$('#instagram').on('pageinit', function() {
+                   
+                   var tag = "dishes_pics",
+                   
+                   url ="https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=dd55a33c284e4d99b2f703f3e2bdaf53",
+                   
+                   
+                   screenOutput = function(info){
+                   
+                   
+                   console.log(info);
+                   
+                   
+                   $("#data-msg").html("<h2>Instagram results:</h2>");
+                   
+                   
+                   $.each(info.data, function(index, photo) {
+                          
+                          var pic = "<figure><img src='" + photo.images.standard_resolution.url + "'alt='" + photo.user.id + "' />" + "<h3>" + "<figcaption>" + photo.user.full_name  + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + photo.likes.count + "&nbsp;" + "&hearts;" + "'s" + "</figcaption>" + " </h3>" + "</figure>";
+                          $("#data-output").append(pic);
+                          });
+                   };
+                   $.getJSON(url, screenOutput);
+                   
+                   
+                   });
 
 
 //GOOGLE MAPS
+var google;
+var map;
 
 function initialize()
 {
-var mapProp = {
-  center:new google.maps.LatLng(31.586040, -97.086380),
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-var map=new google.maps.Map(document.getElementById("map-canvas")
-  ,mapProp);
-
-
-google.maps.event.addDomListener(window, 'load', initialize);
+    var mapProp = {
+    center:new google.maps.LatLng(31.586040, -97.086380),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+    
+    
+    google.maps.event.addDomListener(window, 'load', initialize);
 };
+
+//NATIVE FEATURES
+
+var pictureSource,
+destinationType,
+runAcc,
+runBrowser;
+
+// device APIs are available
+function onDeviceReady() {
+    
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+    $("#accelerometer").on("pageinit", runAcc);
+    $("#browser").on("pageinit", runBrowser);
+    
+}
+// Wait for device API libraries to load
+document.addEventListener("deviceready",onDeviceReady,false);
+
+//CAMERA
+
+// Called when a photo is successfully retrieved
+function onPhotoDataSuccess(imageData) {
+    
+    console.log(imageData);
+    
+    // Get image handle
+    var smallImage = document.getElementById('smallImage');
+    
+    // Unhide image elements
+    smallImage.style.display = 'block';
+    
+    // Show the captured photo
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// Called when a photo is successfully retrieved
+function onPhotoURISuccess(imageURI) {
+    
+    // Uncomment to view the image file URI
+    console.log(imageURI);
+    
+    // Get image handle
+    var largeImage = document.getElementById('largeImage');
+    
+    // Unhide image elements
+    largeImage.style.display = 'block';
+    
+    // Show the captured photo
+    largeImage.src = imageURI;
+}
+
+// Called if something bad happens.
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+
+// A button will call this function
+function capturePhoto() {
+    
+    // Take picture and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+                                destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+function capturePhotoEdit() {
+    // Take picture , allow edit, and retrieve image as base64-encoded string
+    
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+                                destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+
+function getPhoto(source) {
+    
+    // Retrieve image file location from specified source
+    
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+                                destinationType: destinationType.FILE_URI,
+                                sourceType: source });
+}
+
+
+//ACCELEROMETER
+
+function onSuccess(acceleration) {
+    alert('Accelerometer has loaded!');
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+
+// onError: Failed to get the acceleration
+function onError() {
+    alert('onError!');
+}
+
+var runAcc = function(){
+    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+};
+
+//IN APP BROWSER
+
+// Global InAppBrowser reference
+var iAppRef = null;
+
+function iAppLoadStart(event) {
+    alert(event.type + ' - ' + event.url);
+}
+
+function iAppLoadStop(event) {
+    alert(event.type + ' - ' + event.url);
+}
+
+function iAppLoadError(event) {
+    alert(event.type + ' - ' + event.message);
+}
+
+function iAppClose(event) {
+    alert(event.type);
+    iAppRef.removeEventListener('loadstart', iAppLoadStart);
+    iAppRef.removeEventListener('loadstop', iAppLoadStop);
+    iAppRef.removeEventListener('loaderror', iAppLoadError);
+    iAppRef.removeEventListener('exit', iAppClose);
+}
+
+// device APIs are available
+
+var runBrowser = function(){
+    iAppRef = window.open('http://www.fullsail.edu', '_blank', 'location=yes');
+    iAppRef.addEventListener('loadstart', iAppLoadStart);
+    iAppRef.addEventListener('loadstop', iAppLoadStop);
+    iAppRef.removeEventListener('loaderror', iAppLoadError);
+    iAppRef.addEventListener('exit', iAppClose);
+};
+
+
+
 
