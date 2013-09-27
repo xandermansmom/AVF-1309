ee92f7f137ee2a5134232915a7affb82a39345ce
Michele Laramore <xandermansmom@fullsail.edu>
2013-09-27 15:41:19 -0500

    Youtube, still need mashups not saved because not working properly

--- ios/www/js/main.js
+++ ios/www/js/main.js
@@ -51,12 +51,59 @@ function initialize()
     google.maps.event.addDomListener(window, 'load', initialize);
 };
 
+//YOUTUBE
+
+//  This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//  This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+                           height: '390',
+                           width: '640',
+                           videoId: 'Qq6q-AUxdKs',
+                           playerVars: { 'autoplay': 0, 'controls': 1, 'playlist':['Pf1frjJdrNc', 'KhdeKV7aWwc', '-t7Hwj8E7ME', 'AGGjubzw5ms']},
+                           events: {
+                           'onReady': onPlayerReady,
+                           'onStateChange': onPlayerStateChange
+                           }
+                           });
+}
+
+//  The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+
 //NATIVE FEATURES
 
 var pictureSource,
 destinationType,
 runAcc,
 runBrowser,
+playVideo,
 options;
 
 // device APIs are available
@@ -68,6 +115,7 @@ function onDeviceReady() {
     
     $("#accelerometer").on("pageinit", runAcc);
     $("#browser").on("pageinit", runBrowser);
+    $("#youTube").on("pageinit", playVideo);
     
 }
 // Wait for device API libraries to load
